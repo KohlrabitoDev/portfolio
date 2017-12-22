62dd042b6e3000b79bfd852ae0f312f902896b3f
Kenneth Rhodes <kennethrhodes8213@gmail.com>
2017-12-21 22:07:20 -0500

    Added sidebar.

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-<!-- Index Page -->
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>Kenneth Rhodes</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Styles -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"> <!-- Bootstrap for simplicity -->
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+	
+		<span class="openMenu"><a href="#" onclick="openNav();">&#9776;</a></span>
+		<nav id="mySideNav" class="sidenav">
+			<a href="#" class="closeBtn" onclick="closeNav();">&times;</a>
+			
+			<a href="#">Link One</a>
+			<a href="#">Link Two</a>
+			<a href="#">Link Three</a>
+		</nav>
+	
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+		
+		<!-- Custom Scripts -->
+		<script src="js/main.js"></script>
+	</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// Main JavaScript file
+// For sideNav, https://www.w3schools.com/howto/howto_js_sidenav.asp
+
+const openNav = () =>{
+	document.getElementById("mySideNav").style.width = "250px";
+}
+
+const closeNav = () =>{
+	document.getElementById("mySideNav").style.width = "0";
+}
