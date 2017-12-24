e3be3b81b3c299bff5db38af49d3add8d16c4713
Kenneth Rhodes <kennethrhodes8213@gmail.com>
2017-12-24 09:02:31 -0500

    Added a header image, top arrow, and about.

--- index.php
+++ index.php
@@ -17,30 +17,43 @@
 					<a href="#" class="brand">Kenneth Rhodes</a><a href="#" class="closeBtn" onclick="event.preventDefault(); closeNav();">&times;</a>
 				</div>
 				
-				<a href="#">Top</a><!-- Remember to move this to a bottom right arrow. -->
 				<a href="#about">About</a>
-				<a href="#blog">Blog</a>
+				<!--<a href="#blog">Blog</a>-->
 				<a href="#contact">Contact</a>
 				
-				<!-- Add some buttons like: inverted color scheme, night mode,  -->
+				<!-- Add some buttons like: inverted color scheme, night mode, left handed,  -->
 			</div>
 		</nav>
 		
+		<!-- I made this top button :D -->
+		<a id="topButton" href="#">^</a>
+		
 		<header>
 			<h3>Let's Create A Website, <span class="text-success">Together</span></h3>
+			
+			<!-- Create Next Button -->
 		</header>
 		
 		<section id="about">
 			<div class="container-fluid">
-			
+				<h4 class="text-center">Hello, pleased to meet you.</h4>
+				<p class="text-center">
+					&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac consequat felis, quis accumsan ligula. Praesent efficitur sollicitudin volutpat. Fusce id pellentesque arcu, eget elementum orci. In justo magna, auctor sit amet luctus a, vulputate sagittis nisi. Maecenas volutpat nunc sit amet dolor aliquet, et mollis elit lobortis. Donec dapibus ullamcorper ex, eget efficitur libero imperdiet sed. Pellentesque ut dui blandit, vulputate ligula at, tempor augue. Nulla massa ante, iaculis quis mattis ac, ullamcorper eu mi. Praesent rutrum pellentesque felis eu finibus. Pellentesque at sodales ex. Nam eget tincidunt eros, eget lacinia augue. Donec sed leo eget purus interdum sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+				</p>
 			</div>
 		</section>
 		
-		<section id="blog">
+		<!--<section id="blog">
 			<div class="container-fluid">
-			
+				<h4>Sorry, I don't have a blog right now!</h4>
 			</div>
-		</section>
+		</section>-->
+		
+		<!--<section id="warning">
+			<div class="container-fluid">
+				<h4 class="text-center">WARNING: </h4>
+			</div>
+		</section>-->
 		
 		<section id="contact">
 		
