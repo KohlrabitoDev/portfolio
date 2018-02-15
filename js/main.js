cffbfb8f6430b19a591917a8724a938edcfe1678
Kenneth Rhodes <kennethrhodes8213@gmail.com>
2018-02-15 11:40:46 -0500

    More code reformatting.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $(function () {
 });
 
 // For sideNav, https://www.w3schools.com/howto/howto_js_sidenav.asp
-
 const openNav = () =>{
 	if ($(window).width() > 2000){
 		document.getElementById("mySideNav").style.width = "750px";
@@ -18,3 +17,5 @@ const closeNav = () =>{
 	document.getElementById("mySideNav").style.width = "0";
 	document.getElementById("navTextWrapper").style.display = "none";
 }
+
+// Calculate Contact Form Price
