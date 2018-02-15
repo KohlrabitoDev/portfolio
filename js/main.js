608092b4f577bffddeab051c12aa899babdf5002
Kenneth Rhodes <kennethrhodes8213@gmail.com>
2018-02-15 08:57:32 -0500

    Fixed a lot of responsiveness issues, moved some elements.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ $(function () {
 // For sideNav, https://www.w3schools.com/howto/howto_js_sidenav.asp
 
 const openNav = () =>{
-	document.getElementById("mySideNav").style.width = "250px";
+	if ($(window).width() > 2000){
+		document.getElementById("mySideNav").style.width = "750px";
+	}else{
+		document.getElementById("mySideNav").style.width = "250px";
+	}
 	document.getElementById("navTextWrapper").style.display = "block";
 }
 
