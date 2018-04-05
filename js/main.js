c368211bbd5595860145e934ffee3cf8b8c1ddb2
Kenneth Rhodes <KennethRhodes8213@gmail.com>
2018-04-05 12:23:19 -0400

    Closes #2, closes #41, navbar full screen, and added more buttons, though only night mode works so far.

--- js/main.js
+++ js/main.js
@@ -13,11 +13,7 @@ let navIsOpen = false;
 console.log(`Nav is Open: ${navIsOpen}`);
 
 const openNav = () =>{
-	if ($(window).width() > 2000){
-		document.getElementById("mySideNav").style.width = "750px";
-	}else{
-		document.getElementById("mySideNav").style.width = "250px";
-	}
+	document.getElementById("mySideNav").style.width = "100%";
 	document.getElementById("navTextWrapper").style.display = "block";
 	navIsOpen = true;
 	console.log(`Nav is Open: ${navIsOpen}`);
@@ -31,6 +27,48 @@ const closeNav = () =>{
 		console.log(`Nav is Open: ${navIsOpen}`);
 	}
 }
+
+// Actions
+// Turn on indicator in navbar
+let actionIsOn = false;
+$(".actionLink").click(function(){
+	if (actionIsOn == false){
+		$(this).css("background-color", "lightgreen");
+		$(this).css("color", "#fff");
+		actionIsOn = true;
+	}else if (actionIsOn){
+		$(this).css("background-color", "#2b2b2b");
+		$(this).css("color", "#818181");
+		actionIsOn = false;
+	}
+});
+
+// Action Functionality
+// Night Mode
+let nightModeOn = false;
+const nightMode = () =>{
+	if (nightModeOn == false){
+		$("#nightMode").css("display", "block");
+		$("body").css("background-color", "#000");
+		$("body").css("color", "#fff");
+		$("h2.sectionHeading").css("color", "#fff");
+		nightModeOn = true;
+		console.log(`Night Mode is On: ${nightModeOn}`);
+	}else if (nightModeOn == true){
+		$("#nightMode").css("display", "none");
+		$("body").css("background-color", "#f4f4f4");
+		$("body").css("color", "#000");
+		$("h2.sectionHeading").css("color", "#333");
+		nightModeOn = false;
+		console.log(`Night Mode is On: ${nightModeOn}`);
+	}
+}
+
+// Left Handed
+
+
+// Cookies
+
 
 // Close Cookie Banner
 let cookieBannerIsClosed = false;
