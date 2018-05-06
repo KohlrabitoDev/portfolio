f4b58d0e76ef74a0c9485050a99d7eb331a1b65c
Kenneth Rhodes <kennethrhodes8213@gmail.com>
2018-05-06 12:11:19 -0400

    Closes #48

--- js/main.js
+++ js/main.js
@@ -33,6 +33,92 @@ $(".navLink").click(function(){
 	closeNav();
 });
 
+// Price Estimate
+let visitorsAmtEle = document.getElementById("visitorsAmt");
+let numOfPagesEle = document.getElementById("numOfPages");
+let finishEle = document.getElementById("finish");
+
+let yearlyEle = document.getElementById("yearly");
+let yearly = 0;
+let monthlyEle = document.getElementById("monthly");
+let monthly = 0;
+let oneTimeEle = document.getElementById("oneTime");
+let oneTime = 0;
+let totalEle = document.getElementById("total");
+let total = 0;
+
+const updatePrice = () =>{
+	// Update variables
+	let visitorsAmt = visitorsAmtEle.value;
+	let numOfPagesVal = numOfPagesEle.value;
+	let numOfPages = parseInt(numOfPagesVal, 10);
+	let finish = finishEle.value;
+	
+	// Echo to console
+	console.log(`Visitors: ${visitorsAmt}`);
+	console.log(`Pages: ${numOfPages}`);
+	console.log(`Finish in: ${finish}`);
+	
+	// Switch to prices
+	if(visitorsAmt == "OneToFifty"){
+		yearly = 12;
+		monthly = 3.99;
+		oneTime = 5;
+	}else if(visitorsAmt == "FiftyoneToTwoHundred"){
+		yearly = 12;
+		monthly = 6.99;
+		oneTime = 10;
+	}else if(visitorsAmt == "OverThreeHundred"){
+		yearly = 12;
+		monthly = 9.99;
+		oneTime = 50;
+	}
+	
+	// Switch finish
+	if(finish == "30Plus"){
+		oneTime = oneTime + (numOfPages * 5);
+	}else if(finish == "14Days"){
+		oneTime = oneTime + 30 + (numOfPages * 10);
+	}else if(finish == "7Days"){
+		oneTime = oneTime + 50 + (numOfPages * 11);
+	}
+	
+	// If num of pages is less than or equal to zero, this will show.
+	if(numOfPages <= 0){
+		numOfPages = "Enter a valid number of pages."
+	}
+	
+	// Price modifiers
+	let useOwnDomain = document.getElementById("useOwnDomain").checked;
+	let useOwnHosting = document.getElementById("useOwnHosting").checked;
+	let monthlyMaintenance = document.getElementById("monthlyMaintenance").checked;
+	
+	if(useOwnDomain){console.log("useOwnDomain is true"); yearly = 0;}
+	if(useOwnHosting){console.log("useOwnHosting is true"); monthly = 0;}
+	if(monthlyMaintenance){console.log("monthlyMaintenance is true"); monthly = monthly + 12}
+	//Round all numbers to two decimals
+		yearly = Math.round(yearly * 100) / 100;
+		monthly = Math.round(monthly * 100) / 100;
+		oneTime = Math.round(oneTime * 100) / 100;
+	
+	//Total
+		total = yearly + monthly + oneTime;
+		total = Math.round(total * 100) / 100;
+		
+	
+	
+	
+	// Post
+	yearlyEle.innerHTML = yearly;
+	monthlyEle.innerHTML = monthly;
+	oneTimeEle.innerHTML = oneTime;
+	if (yearly >= 0 && monthly >= 0 && oneTime > 0 && finish != "Finish it in..."){
+		totalEle.innerHTML = total;
+	}
+}
+
+
+
 // Actions
 // Turn on indicator in navbar
 let actionIsOn = false;
